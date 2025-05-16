fbca3b01aa10abeaf07ceaf6a3aef95398290ad7
Nitish Kumar <iamnitishsah12@gmail.com>
2025-05-16 12:54:35 +0530

    Update settings.py

--- NiSan-Backend/NiSan/settings.py
+++ NiSan-Backend/NiSan/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split() or ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -93,7 +93,8 @@ DATABASES = {
 }
 
 database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(database_url)
+if database_url:
+    DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
